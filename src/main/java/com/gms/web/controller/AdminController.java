172ab51d33212ce4349d96eb56d41de343b7cbec
이슬 <lina900904@gmail.com>
2018-09-11 10:40:15 +0900

    ing

--- src/main/java/com/gms/web/controller/AdminController.java
+++ src/main/java/com/gms/web/controller/AdminController.java
@@ -7,5 +7,20 @@ import org.springframework.web.bind.annotation.SessionAttributes;
 @Controller
 @RequestMapping("/board")
 public class AdminController {
+	public static void main(String[] args) {
+		
+	}
+	class Box<T>{
+		T item;
+
+		public T getItem() {
+			return item;
+		}
+
+		public void setItem(T item) {
+			this.item = item;
+		}
+	}
 
 }
+
